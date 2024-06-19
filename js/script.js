72f63b54232bad396915857986a03aadcf751308
Leejaeho1234 <ljh2735294@naver.com>
2024-06-20 01:37:45 +0900

    반복문으로 정답 버튼 클릭시 문제 fadeOut, fadeIn 기능 개발

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // const qTitle = [
 
 // ]
+
+// 정답 버튼 누를 시 다음 문제 fadeIn
+for(let i = 1; i < 5; i++){
+    $(`.q-${i} .answer-btn`).click(() => {
+        $(`.q-${i}`).fadeOut();
+        setTimeout(() => {
+            $(`.q-${i + 1}`).fadeIn();
+        }, 700);
+    })
+}
